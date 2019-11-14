e673cc73520f316a6042e796e0db4c89371b5fed
ElijahKz <milmax.duque@uao.edu.co>
2019-11-14 18:49:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+<body>
+        <div class="container fluid d-flex justify-content-center ">
+
+                <div>
+                    <div class="row sm-5 mt-5 ">
+        
+                        <div class="col mt-5 shadow-sm p-3 mb-5 bg-white rounded">
+                            <h5 class="d-flex justify-content-center"> Scegli il gioco</h5>
+                            <div class="form-group mt-5">                               
+                               <center> <a href="https://es.educaplay.com/es/recursoseducativos/4898534/html5/le_feste_italiana.htm#!"><input class="btn btn-primary" type="button" value="minestra di lettere"></a></center>                            
+                            </div>
+                            <div class="form-group">                              
+                                    <center> <a href="https://kahoot.it/">  <input class="btn btn-primary" type="button" value="Domande e risposte"></a></center> 
+                            </div>
+                         
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!-- The core Firebase JS SDK is always required and must be listed first -->
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+
+</body>
+?>
